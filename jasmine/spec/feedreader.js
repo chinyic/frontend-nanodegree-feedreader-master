04f6691a05a8fe7195d542ade1ffb68bf28e65e1
Chinyi <chinyi.cc@gmail.com>
2018-07-10 15:17:41 -0700

    menu hidden test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -46,14 +46,22 @@ $(function() {
             allFeeds.forEach(function(feed){
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
-            })
-         })
+            });
+         });
 
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function(){
 
+      var body = $('body');
+      var menuButton = document.querySelector('.menu-icon-link')
+
+      it('is hidden by default', function() {
+        expect(body[0].classList.contains('menu-hidden')).toBe(true);
+      });
+    });
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
